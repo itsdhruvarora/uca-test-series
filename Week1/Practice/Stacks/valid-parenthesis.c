f974350fde836e5e7d47c31cd65ec0c2c9971f76
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2023-12-18 15:37:15 +0530

    Create valid-parenthesis.c

---
+++ Week1/Practice/Stacks/valid-parenthesis.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool isValid(char* s) {
+    int st[10000];
+    int top = -1;
+    
+    for (int i = 0; i < strlen(s); i++) {
+        if (top == -1) {
+            st[++top] = s[i];
+        } else if (st[top] == '(' && s[i] == ')') {
+            top--;
+        } else if (st[top] == '[' && s[i] == ']') {
+            top--;
+        } else if (st[top] == '{' && s[i] == '}') {
+            top--;
+        } else {
+            st[++top] = s[i];
+        }
+    }
+
+    return top == -1;
+}
+
+
+
