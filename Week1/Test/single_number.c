ae989d496c0f714f306c08276e75f03013b4e91b
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2023-12-30 13:20:09 +0530

    Create single_number.c

---
+++ Week1/Test/single_number.c
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/single-number/
+
+int singleNumber(int* nums, int numsSize) {
+    int ans = 0;
+    for(int i=0; i<numsSize; i++){
+        ans ^= nums[i];
+    }
+
+    return ans;
+}
